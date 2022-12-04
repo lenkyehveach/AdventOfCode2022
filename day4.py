435280743f4512bd840009f0e3fac99d31d1e90b
lenkyehveach <lenkiewiczjulian@gmail.com>
2022-12-04 13:38:37 +0000

    day4.1 complete

---
+++ day4.py
@@ -0,0 +1,23 @@
+with open("input4.txt","r") as f: 
+  data = [line.strip() for line in f.readlines()]
+
+#print(data)
+def checkElfRange(elf1, elf2): 
+  e11, e12 = elf1.split("-")
+  e21, e22 = elf2.split("-")
+
+  #check if elf 1 is contained by elf2
+  if int(e11) >= int(e21) and int(e12) <= int(e22): 
+    return True
+  else:
+    return False
+
+overlaps = 0
+
+for i in range(len(data)): 
+  elf1, elf2 = data[i].split(",")
+  
+  if checkElfRange(elf1, elf2) or checkElfRange(elf2, elf1):
+    overlaps += 1
+
+print(overlaps)
