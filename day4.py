b6f667e43e92fc074f6d5178ba632ef264698233
lenkyehveach <lenkiewiczjulian@gmail.com>
2022-12-04 14:27:21 +0000

    day4.2 complete

--- day4.py
+++ day4.py
@@ -21,3 +21,25 @@ for i in range(len(data)):
     overlaps += 1
 
 print(overlaps)
+
+
+def checkAnyOverlap(elf1, elf2): 
+  e11, e12 = elf1.split("-")
+  e21, e22 = elf2.split("-")
+
+  if int(e21) <= int(e11) <= int(e22) or  int(e21) <= int(e12) <= int(e22): 
+    return True
+  else:
+    return False
+
+any_overlaps = 0
+
+for i in range(len(data)): 
+  elf1, elf2 = data[i].split(",")
+  
+  if checkAnyOverlap(elf1, elf2) or checkAnyOverlap(elf2, elf1):
+    any_overlaps += 1
+
+print(any_overlaps)
+  
+
