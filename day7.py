17ab58cef5db7220dda0d7b8f9b437952c7ea1b3
lenkyehveach <lenkiewiczjulian@gmail.com>
2022-12-07 21:02:41 +0000

    day7.1 complete! Hardest one so far

--- day7.py
+++ day7.py
@@ -1,47 +1,108 @@
+ 
+from pprint import pprint
+
 with open("input7.txt") as f: 
   data = [d.strip() for d in  f.readlines()]
 
-dirs = {"/": 0}
+data = data[2:]
+blocks = []
+start = 0
 
-for line in data: 
-  if "dir" in line: 
-    dir_name = line.split(" ")[1]
-    dirs[dir_name] = len(dirs.keys())
-  
-files = [[] for _ in range(len(dirs.keys()))]
 
-wip = []
-current = 0
+for i in range(len(data)+1): 
+  if i == len(data):
+    
+    blocks.append(data[start: i])
+    break
+  line = data[i]
+  if "$" in line and "$" not in data[i-1]:
+    blocks.append(data[start: i])
+    start = i
 
-for line in data: 
-  print(line)
-  if "$" in line: 
-    if "cd" in line: 
-      if "/" in line: 
+dirs = dict()
+path = ["_"]
+
+def parseBlock(block): 
+  wip = []
+  for line in block: 
+    if "$" in line:
+      command = line.split(" ")
+      
+      if line == "$ ls": 
         continue
 
-      elif ".." in line:
-        continue 
-
+      if command[2] == "..": 
+        path.pop()
       else: 
-        files[current] = wip
-        wip = []
-        
-        current = dirs[line.split(" ")[2]]
-  
-  else: 
-    if "dir" in line: 
-      wip.append(line.split(" ")[1])
+        path.append(command[2])
+
     else: 
-      size, name = line.split(" ")
-      wip.append(int(size))
-  
+      if "dir" in line: 
+        wip.append(line.split(" ")[1])
+      else: 
+        wip.append(int(line.split(" ")[0]))
 
-c = 0
-for x in files: 
-  print(c, ": ", x )
-  c +=1 
+  if len(path) == 1 :
+    dirs["_"] = wip 
 
-print(dirs)
+  else:
+    dirs[("/").join(path)] = wip
 
-#print(dirs)
+c= 0
+for block in blocks: 
+
+  parseBlock(block)
+
+
+  c+= 1
+
+visited = {}
+
+def path_length(path): 
+  return len(path.split("/"))
+
+for i in (sorted(dirs.keys(), reverse=True, key=path_length)):
+  # print(i)
+  # print(dirs[i])
+  # print(f'{".":.^20}')
+  if all([type(x) == int for x in dirs[i]]): 
+    visited[i] = sum(dirs[i])
+  else: 
+    test = []
+    for file in dirs[i]: 
+      if type(file) == str:
+        test.append(visited[i+"/"+file])
+      else: 
+        test.append(file)
+    
+    visited[i] = sum(test)
+
+
+#pprint(visited)
+poss = []
+for key, value in visited.items(): 
+  if value <= 100000: 
+    poss.append(value)
+
+print(sum(poss))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
