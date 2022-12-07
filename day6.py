3f704bd157098ed541532da34a321c1694196ed0
lenkyehveach <lenkiewiczjulian@gmail.com>
2022-12-07 13:10:49 +0000

    day6.2 complete; couldn't submit yesterday due to illness :(

--- day6.py
+++ day6.py
@@ -1,12 +1,20 @@
 with open("input6.txt", "r") as f:
   buffer = f.read().strip()
 
-print(len(buffer))
-
-
 for i in range(3, len(buffer)): #
   last4 = set([buffer[i-3], buffer[i-2], buffer[i-1], buffer[i]])
-  print(last4)
+  
   if len(last4) == 4: 
+    print(i + 1)
+    break
+
+# -------------------------------
+
+for i in range(13, len(buffer)): #
+
+  signal = [buffer[i-n] for n in range(13,-1, -1)]
+  last4 = set(signal)
+
+  if len(last4) == 14: 
     print(i + 1)
     break
