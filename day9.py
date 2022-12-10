45e56779c79325e7cf7453dcf541fe8a668143a8
lenkyehveach <lenkiewiczjulian@gmail.com>
2022-12-10 13:30:33 +0000

    day9.1 complete

---
+++ day9.py
@@ -0,0 +1,106 @@
+# import numpy as np 
+
+# with open("test.txt", "r") as f: 
+#   data = [line.strip() for line in f.readlines()]
+
+
+# head = np.zeros((6,6)) 
+# head[5, 0] = 1
+# print(head)
+
+# tail = np.zeros((6,6))
+# tail[5, 0] = 2
+
+# def move(grid, direction, steps, rope=1  ): 
+
+#   for _ in range(int(steps)): 
+    
+#     position = np.where(grid==rope)
+#     row = position[0]
+#     col = position[1]
+#     grid[row, col] = 0 #rope moves
+
+#     if direction == "U": 
+#       grid[row-1, col] = rope
+    
+#     elif direction == "D": 
+#       grid[row+1, col] = rope
+    
+#     elif direction == "L": 
+#       grid[row, col-1] = rope
+    
+#     elif direction == "R": 
+#       grid[row, col+1] = rope
+
+#   return grid 
+
+
+# for i in data: 
+#   direction, steps = i.split(" ")
+
+#   head = move(head, direction, steps, 1)
+#   print(direction, steps)
+#   print(head)
+  
+
+with open("input9.txt", "r") as f: 
+  data = [line.strip() for line in f.readlines()]
+
+directions = [line.split(" ")[0] for line in data ]
+steps = [int(line.split(" ")[1]) for line in data ]
+
+x_h, x_t = 0, 0 
+y_h, y_t = 0, 0
+
+locations = [(0, 0)]
+
+for d, step in zip(directions, steps): 
+
+  print(d, step)
+  print("_----")
+
+  for s in range(step): 
+    if d == "U": 
+      y_h += 1
+    elif d == "D":
+      y_h -= 1
+    elif d == "R":
+      x_h += 1
+    elif d == "L":
+      x_h -= 1
+
+    print("H ", x_h, y_h)
+      
+    if x_h - x_t > 1:
+      x_t += 1
+      if y_h > y_t: 
+        y_t += 1
+      elif y_h < y_t: 
+        y_t -= 1
+    elif  x_h - x_t < -1: 
+      x_t -= 1
+      if y_h > y_t: 
+        y_t += 1
+      elif y_h < y_t: 
+        y_t -= 1
+    
+
+    if y_h - y_t > 1: 
+      y_t += 1
+      if x_h > x_t: 
+        x_t +=1 
+      elif x_h < x_t:
+        x_t -=1
+    elif  y_h - y_t < -1: 
+      y_t -= 1 
+      if x_h > x_t: 
+        x_t +=1 
+      elif x_h < x_t:
+        x_t -=1
+    print("t: " , x_t, y_t)
+
+    if (x_t, y_t) not in locations: 
+      locations.append((x_t, y_t))
+
+print(locations)
+print(len(locations))
